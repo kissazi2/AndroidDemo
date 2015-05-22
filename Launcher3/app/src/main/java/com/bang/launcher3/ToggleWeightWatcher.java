6f8b55fc2657801146967395a1c2e4537912995a
陈喆榕 <bang@chenzherongdeMacBook-Pro.local>
2015-05-22 16:18:33 +0800

    first commit

---
+++ Launcher3/app/src/main/java/com/bang/launcher3/ToggleWeightWatcher.java
@@ -0,0 +1,7 @@
+package com.bang.launcher3;
+
+import android.app.Activity;
+
+public class ToggleWeightWatcher extends Activity {
+
+}
---
+++ Launcher3/app/src/main/java/com/bang/launcher3/WidgetAdder.java
@@ -0,0 +1,7 @@
+package com.bang.launcher3;
+
+import android.app.Activity;
+
+public class WidgetAdder extends Activity {
+
+}
